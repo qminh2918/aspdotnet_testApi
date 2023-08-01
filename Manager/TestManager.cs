f48801f3766266ab2305dd8aef13a5b0584f9c4d
DESKTOP-74CIOSO\MINH <qmin123443@gmail.com>
2023-08-02 00:54:03 +0700

    minh commit

---
+++ Manager/TestManager.cs
@@ -0,0 +1,12 @@
+﻿
+namespace TestApi.Manager
+{
+    public interface ITestManager
+    {
+
+    }
+    public class TestManager : ITestManager
+    {
+        //public async Task<ApiResponse<List<>>
+    }
+}
